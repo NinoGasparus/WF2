7b9cda8b4d6bd041a744d27be03d0be34de3e687
NinoGasparus <ngasparus@gmail.com>
2025-06-25 17:01:35 +0200

    Nothign

---
+++ func.h
@@ -0,0 +1,3 @@
+
+
+void compute(uint8_t store, int x, int y);
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
+#include "func.h"
 
-
-
-
-
+int x = 30;
+int y = 15;
+uint8_t mem = new uint8_t[x*y];
 int main(){
+	
 
   return 0;
 
